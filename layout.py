74aef1df46fcb1b0e88e85269a23fc746f484762
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2024-11-25 01:37:54 -0500

    align words on baseline

--- layout.py
+++ layout.py
@@ -17,6 +17,7 @@ class Tag:
 class Layout:
     def __init__(self, tokens):
         self.display_list = []
+        self.line = []
         self.cursor_x = HSTEP
         self.cursor_y = VSTEP
         self.size = 12
@@ -25,6 +26,25 @@ class Layout:
 
         for tok in tokens:
             self.token(tok)
+
+        self.flush()
+
+    def flush(self):
+        if not self.line:
+            return
+        metrics = [self.font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+
+        baseline = self.cursor_y + 1.25 * max_ascent
+
+        for x, word, self.font in self.line:
+            y = baseline - self.font.metrics("ascent")
+            self.display_list.append((x, y, word, self.font))
+            max_descent = max([metric["descent"] for metric in metrics])
+            self.cursor_y = baseline + 1.25 * max_descent
+
+        self.cursor_x = HSTEP
+        self.line = []
 
     def token(self, tok):
         if isinstance(tok, Text):
@@ -46,20 +66,26 @@ class Layout:
             self.size += 4
         elif tok.tag == "/big":
             self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
 
     def word(self, word):
-        font = tkinter.font.Font(
+        self.font = tkinter.font.Font(
             family="Georgia",
             size=self.size,
             weight=self.weight,
             slant=self.style,
         )
-        w = font.measure(word)
+        w = self.font.measure(word)
         if self.cursor_x + w >= WIDTH - HSTEP:
-            self.cursor_y += font.metrics("linespace") * 1.25
+            self.flush()
+            self.cursor_y += self.font.metrics("linespace") * 1.25
             self.cursor_x = HSTEP
-        self.display_list.append((self.cursor_x, self.cursor_y, word, font))
-        self.cursor_x += w + font.measure(" ")
+        self.line.append((self.cursor_x, word, self.font))
+        self.cursor_x += w + self.font.measure(" ")
 
 
 def lex(body):
